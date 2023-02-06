0ac3eb5068a5ccd0032e1d6750d5250f5f54e761
Francis KOUAHO <kouahofrancis@gmail.com>
2023-02-06 22:12:02 +0100

    initilisation projet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import React from "react";
 
 export default function RootLayout({
   children,
@@ -6,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
