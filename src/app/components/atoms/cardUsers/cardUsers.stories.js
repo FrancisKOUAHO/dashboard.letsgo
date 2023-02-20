5e8ad43d3e3e790eae80355040a81a28d21dda7f
Voldi Zola <voldi.zola@wister.fr>
2023-02-20 14:07:17 +0100

    card users

---
+++ src/app/components/atoms/cardUsers/cardUsers.stories.js
@@ -0,0 +1,15 @@
+import CardUsers from "./cardUsers";
+import Image from "next/image";
+import Zola from "../../../assets/profile-pic.png"
+export const BaseCardUsers = () =>{
+    return(
+        <CardUsers className="c-cardUsers" name="Zola Voldi" role="admin">
+            <Image className="cardUsers-Image" src={Zola} width="100" height="100"/>
+        </CardUsers>
+        )
+}
+
+export default {
+    title: "Atoms/CardUsers",
+    component: CardUsers,
+}
