6f44dffc9915e8b8f03ff6c664f3974014d06dcc
Voldi Zola <voldi.zola@wister.fr>
2023-03-07 23:16:08 +0100

    Bug sur le NaN price  fixé

--- app/partner/dashboard/page.tsx
+++ app/partner/dashboard/page.tsx
@@ -1,14 +1,16 @@
 "use client"
 
 import React from "react";
-import {AiOutlineDesktop, AiOutlineShoppingCart, AiOutlineUser} from "react-icons/ai";
-import {IsAuthorized} from "@/utils/auth";
-import LayoutCustom from "@/layouts/layoutCustom";
+import {AiOutlineEuro, AiOutlineShoppingCart, AiOutlineUser} from "react-icons/ai";
 import CardStats from "@/components/atoms/cardstats/cardStats";
+import LoadingSpinner from "@/components/atoms/loadingspinner/loadingSpinner";
+import {IsAuthorized} from "@/utils/auth";
+import {useReservations} from "@/hooks/useReservations";
+import LayoutCustom from "@/layouts/layoutCustom";
 
 const icons = {
   shoppingCart: <AiOutlineShoppingCart  className="AiOutlineShoppingCart"/>,
-  desktop: <AiOutlineDesktop className="AiOutlineDesktop"/>,
+  euro: <AiOutlineEuro className="AiOutlineDesktop"/>,
   user: <AiOutlineUser className="AiOutlineUser" />
 };
 
@@ -16,23 +18,38 @@ const icons = {
 const Page = () => {
   const authorized = IsAuthorized('partner');
 
+  const {data, status, error} = useReservations()
+  const totalReservations = data?.data.length
+
+  let  totalPrice = 0;
+
+  for (let i = 0; i < totalReservations; i++) {
+    const reservation = data?.data[i];
+    const price = parseFloat(reservation.total_price);
+    totalPrice += price;
+  }
+
   if (!authorized) {
     return <div>Not Authorized</div>;
   }
 
+  if (!authorized) return <LayoutCustom><div className="flex justify-center items-center h-screen">Not Authorized</div></LayoutCustom>
+  if (status === "loading") return <LayoutCustom><div className="flex justify-center items-center h-screen"><LoadingSpinner/></div></LayoutCustom>
+  if (error === "error") return <LayoutCustom><div className="flex justify-center items-center h-screen">Erreur...</div></LayoutCustom>
+
   return (
-    <LayoutCustom>
-      <div className="c-categories">
-        <h2>{"Bilan d'affaires"}</h2>
-        <div className="c-users__users">
-          <div className="container-card">
-            <CardStats className="c-cardStats" icon={icons.shoppingCart} numberofpourcents="0" titleCards="Nombre de réservations" />
-            <CardStats className="c-cardStats" icon={icons.desktop} numberofpourcents="10" titleCards="Nombre d'offres" />
-            <CardStats className="c-cardStats" icon={icons.user} numberofpourcents="0" titleCards="Nombre de visiteurs" />
+      <LayoutCustom>
+        <div className="c-categories">
+          <h2>{"Bilan d'affaires"}</h2>
+          <div className="c-users__users">
+            <div className="container-card">
+              <CardStats className="c-cardStats" icon={icons.shoppingCart} number={totalReservations} numberofpourcents="0" titleCards="Nombre de réservations" />
+              <CardStats className="c-cardStats" icon={icons.euro} number={totalPrice} numberofpourcents="0" titleCards="Chiffres d'affaires" />
+              <CardStats className="c-cardStats" icon={icons.user} number={totalReservations} numberofpourcents="0" titleCards="Nombre de visiteurs" />
+            </div>
           </div>
         </div>
-      </div>‡‡
-    </LayoutCustom>
+      </LayoutCustom>
   )
 }
 
