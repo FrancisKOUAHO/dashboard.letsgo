c9114c3b3063c3ece0053dba8d02217c5dde53dc
Francis KOUAHO <kouahofrancis@gmail.com>
2023-03-07 15:45:58 +0100

    resolves build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
+const path = require("path");
+const nextConfig = require("./next.config");
 module.exports = {
   apps: [
     {
       name: 'dashboard.letsgoeurope.fr',
       instances: "max",
       exec_mode: "cluster",
-      script: 'node_modules/.bin/next',
+      script: path.resolve('./node_modules/.bin/next'),
       args: 'start -p 3030',
       watch: true,
       env_production: {
@@ -13,4 +15,5 @@ module.exports = {
       },
     },
   ],
+  ...nextConfig,
 };
