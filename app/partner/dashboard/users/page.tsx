40329777b2fdce367fb3dad2d17c6b5f125bd318
Francis KOUAHO <f.kouaho@skipperndt.com>
2024-01-21 12:09:59 +0100

    fix(ui): fixed bug partner

--- app/partner/dashboard/users/page.tsx
+++ app/partner/dashboard/users/page.tsx
@@ -9,39 +9,37 @@ import useIsAuthorized from "@/utils/auth";
 
 
 const Page = () => {
-  const isAuthorized = useIsAuthorized('partner');
+    const isAuthorized = useIsAuthorized('partner');
 
-  const {data, status, error} = useUsers()
+    const {data, status, error} = useUsers()
 
-  if (!isAuthorized) return <LayoutCustom><div className="flex justify-center items-center h-screen">Not Authorized</div></LayoutCustom>
-  if (status === "loading") return <LayoutCustom><div className="flex justify-center items-center h-screen"><LoadingSpinner/></div></LayoutCustom>
-  if (error === "error") return <LayoutCustom><div className="flex justify-center items-center h-screen">Erreur...</div></LayoutCustom>
+    return (
+        <LayoutCustom>
+            <div className="c-users">
+                <h2>{"Tous les utilisateurs"}</h2>
 
-  return (
-    <LayoutCustom>
-      <div className="c-users">
-        <h2>{"Tous les utilisateurs"}</h2>
+                <div className="c-users__users mt-12">
 
-        <div className="c-users__users mt-12">
+                    <div>
+                        <div className="container-card">
+                            {data &&
+                                data.data.map((user: any, index: number) => {
+                                    return (
+                                        <CardUsers className="c-cardUsers" key={index} name={user.full_name}
+                                                   role={user.role}>
+                                            <img className="cardUsers-Image" src={user.photo} alt={user.full_name}
+                                                 width="100" height="100"/>
+                                        </CardUsers>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
 
-          <div>
-            <div className="container-card">
-              {data &&
-                data.data.map((user: any, index: number) => {
-                  return (
-                    <CardUsers className="c-cardUsers" key={index} name={user.full_name} role={user.role}>
-                      <img className="cardUsers-Image" src={user.photo} alt={user.full_name} width="100" height="100"/>
-                    </CardUsers>
-                  )
-                })
-              }
             </div>
-          </div>
-        </div>
-
-      </div>
-    </LayoutCustom>
-  )
+        </LayoutCustom>
+    )
 }
 
 export default Page
