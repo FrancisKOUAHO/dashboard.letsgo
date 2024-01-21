e07f09feb2624bf31b8f4b6d56f9679426c944a9
Francis KOUAHO <f.kouaho@skipperndt.com>
2024-01-21 12:16:09 +0100

    fix(ui): fixed bug partner

--- components/atoms/loadingspinner/loadingSpinner.tsx
+++ components/atoms/loadingspinner/loadingSpinner.tsx
@@ -1,15 +1,10 @@
 import {NextPage} from 'next'
-import {LineWave} from 'react-loader-spinner'
 
 const LoadingSpinner: NextPage = () => {
     return (
-        <LineWave
-            height="80"
-            width="80"
-            wrapperClass={`c-loading-spinner`}
-            ariaLabel="Loading triangle spinner"
-            visible
-        />
+        <p>
+            Loading triangle spinner
+        </p>
 
     );
 };
